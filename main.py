9a32810b4067e743bbd97cb32ba4a5eece971b83
Jacob Sagini <40792254+sultanjacob@users.noreply.github.com>
2026-03-21 19:44:15 +0000

    New update
    updating the main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ load_dotenv("keys.env")
 api_key = os.getenv("GEMINI_API_KEY") 
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-pro')
 
 app = FastAPI()
 PROFILES_DIR = r"D:\MLP\dementia-reminiscence-rag\profiles"
