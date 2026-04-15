fb16124ee4b4b9d2a17e867a00d409a35f182d35
Jacob Sagini <40792254+sultanjacob@users.noreply.github.com>
2026-04-15 23:15:40 +0100

    new update
    updating the supabase.ts

--- remi-mobile/supabase.ts
+++ remi-mobile/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your actual keys from Supabase Settings > API
 const supabaseUrl = 'https://bphmzxsidlxfawqkvksr.supabase.co';
-const supabaseAnonKey = 'PASTE_YOUR_ANON_KEY_HERE';
+const supabaseAnonKey = 'sb_publishable_-RYTM7gdaV_1IE3d6F9GNQ_OEoAH3lY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
