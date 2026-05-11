5067154e9f7f45d33a98a2b1eee45aea4ac36084
Jacob Sagini <40792254+sultanjacob@users.noreply.github.com>
2026-05-11 23:25:03 +0100

    Update _layout.tsx
    change to the header appearance

--- remi-mobile/app/(auth)/_layout.tsx
+++ remi-mobile/app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ 
       tabBarActiveTintColor: '#003366',
-      headerShown: true, // You can change this to false later if you want a custom top header!
+      headerShown: false, // You can change this to false later if you want a custom top header!
     }}>
       
       {/* --- VISIBLE TABS (The User Zone) --- */}
